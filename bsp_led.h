f443bedaccffa3506d17fbc9b56897a4e4518ac0
sudaozhen <sudaozhen@outlook.com>
2019-10-18 22:40:11 +0800

    change led

--- bsp_led.h
+++ bsp_led.h
@@ -28,16 +28,16 @@
 #define LED3_GPIO_PIN   GPIO_Pin_15
 
 //LED1开关
-#define LED1_ON                   GPIO_SetBits(LED1_GPIO_PORT, LED1_GPIO_PIN)
-#define LED1_OFF                  GPIO_ResetBits(LED1_GPIO_PORT, LED1_GPIO_PIN)
+#define LED1_OFF                  GPIO_SetBits(LED1_GPIO_PORT, LED1_GPIO_PIN)
+#define LED1_ON                   GPIO_ResetBits(LED1_GPIO_PORT, LED1_GPIO_PIN)
 #define LED1_TOGGLE               (LED1_GPIO_PORT->ODR ^= LED1_GPIO_PIN)
 //LED2开关
-#define LED2_ON                   GPIO_SetBits(LED2_GPIO_PORT, LED2_GPIO_PIN)
-#define LED2_OFF                  GPIO_ResetBits(LED2_GPIO_PORT, LED2_GPIO_PIN)
+#define LED2_OFF                  GPIO_SetBits(LED2_GPIO_PORT, LED2_GPIO_PIN)
+#define LED2_ON                   GPIO_ResetBits(LED2_GPIO_PORT, LED2_GPIO_PIN)
 #define LED2_TOGGLE               (LED2_GPIO_PORT->ODR ^= LED2_GPIO_PIN)
 //LED3开关
-#define LED3_ON                   GPIO_SetBits(LED3_GPIO_PORT, LED3_GPIO_PIN)
-#define LED3_OFF                  GPIO_ResetBits(LED3_GPIO_PORT, LED3_GPIO_PIN)
+#define LED3_OFF                  GPIO_SetBits(LED3_GPIO_PORT, LED3_GPIO_PIN)
+#define LED3_ON                   GPIO_ResetBits(LED3_GPIO_PORT, LED3_GPIO_PIN)
 #define LED3_TOGGLE               (LED3_GPIO_PORT->ODR ^= LED3_GPIO_PIN)
 
 void LED_GPIO_Config(void);
