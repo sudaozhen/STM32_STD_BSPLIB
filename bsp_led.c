2bc9cafd9bdea8372a9312df60136505aae96d68
sudaozhen <sudaozhen@outlook.com>
2019-10-18 22:40:11 +0800

    change led

--- bsp_led.c
+++ bsp_led.c
@@ -28,14 +28,16 @@ void LED_GPIO_Config(void)
 	
 	//调用外设初始化函数，把配置好的结构体成员写到寄存器里面
 	GPIO_Init(LED1_GPIO_PORT, &GPIO_InitStruct);
-	LED1_OFF;
+
 	//写LED2寄存器
 	GPIO_InitStruct.GPIO_Pin = LED2_GPIO_PIN;
 	GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
-	LED2_OFF;
+
 	//写LED3寄存器
 	GPIO_InitStruct.GPIO_Pin = LED3_GPIO_PIN;
 	GPIO_Init(LED3_GPIO_PORT, &GPIO_InitStruct);
+	LED1_OFF;
+	LED2_OFF;
 	LED3_OFF;
 }
 /**** Copyright (C)2019 Su Daozhen. All Rights Reserved **** END OF FILE ****/
